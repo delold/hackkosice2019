0052722e89ec361a59fa95b812d8f72dce50140d
David Duong <david@duong.cz>
2019-03-30 21:54:19 +0100

    Timer

--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from './PieChart.module.css'
+import moment from 'moment'
 
 import { PieChart, Pie } from 'recharts'
 
@@ -8,7 +9,57 @@ const data01 = [
   { value: 30 },
 ]
 
-const PieComponent = () => {
+const formatDuration = (timestamp) => {
+  let rounded = Math.floor(timestamp / 1000)
+  const seconds = rounded % 60
+
+  rounded = Math.floor(rounded / 60)
+  const minutes = rounded % 60
+  
+  rounded = Math.floor(rounded / 60)
+  const hours = rounded % 24
+
+  const pad = (str) => str > 9 ? str : `0${str}`
+
+  return [pad(hours), pad(minutes), pad(seconds)].join(':')
+}
+
+
+const useInterval = () => {
+  const ref = useRef()
+
+  const [start, setStart] = useState(null)
+  const [timestamp, setTimestamp] = useState(null)
+
+  ref.current = () => {
+    if (!start) {
+      const now = Date.now()
+      setTimestamp(now)
+      setStart(now)
+    } else {
+      setStart(null)
+    }
+  }
+  
+  useEffect(() => {
+    if (start) {
+      const nextTimestamp = Math.floor((timestamp / 1000) + 1) * 1000
+      const delay = (timestamp + 1000) - nextTimestamp
+
+      setTimeout(() => setTimestamp(nextTimestamp), delay)
+    }
+  });
+
+  return [start ? timestamp - start : null, ref.current]
+}
+
+
+
+const PieComponent = ({ debt }) => {
+  const [delay, toggle] = useInterval()
+
+  console.log(delay)
+
   const width = 300
   const height = 300
   return <div className={styles.chart} style={{ width, height }}>
@@ -26,10 +77,10 @@ const PieComponent = () => {
         />
       </PieChart>
     </div>
-    <div className={styles.inside}>
+    <div className={styles.inside} onClick={toggle}>
       <span className={styles.description}>You are working for</span>
       <div className={styles.hours}>
-        12:35:12
+        {formatDuration(delay)}
       </div>
       <span className={styles.active}>hours</span>
     </div>
