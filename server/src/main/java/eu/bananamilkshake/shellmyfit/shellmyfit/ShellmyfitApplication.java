78ff4271d446d8f646edbd653256237b5b193a81
Minh Trieu <mnhtrieu@gmail.com>
2019-03-30 20:48:28 +0100

    initial commit server

---
+++ server/src/main/java/eu/bananamilkshake/shellmyfit/shellmyfit/ShellmyfitApplication.java
@@ -0,0 +1,13 @@
+package eu.bananamilkshake.shellmyfit.shellmyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellmyfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellmyfitApplication.class, args);
+	}
+
+}
